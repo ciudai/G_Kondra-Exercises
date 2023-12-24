52dbe34b56c4da2eb18422259271bc2982a4e52d
ciudai <gabijakondra@gmail.com>
2023-12-24 21:43:21 +0200

    fix

---
+++ PD2/PD2_1.java
@@ -0,0 +1,10 @@
+package PD2;
+
+public class PD2_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("learning Java Now");
+        System.out.println("programming is fun");
+    }
+}
