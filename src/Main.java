669ea91efae65d811790ede8bfa8c25a8522fbe9
ciudai <gabijakondra@gmail.com>
2023-12-24 18:39:44 +0200

    Labas, Vardas!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        String uName = myObj.nextLine();
+        System.out.println("Labas," + uName);
+    }
+}
