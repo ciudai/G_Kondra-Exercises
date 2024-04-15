f4d6929fbab9f85bdabc706edffff3482638d03f
ciudai <gabijakondra@gmail.com>
2024-04-15 11:41:34 +0300

    update

---
+++ PD3/PD3_2.java
@@ -0,0 +1,14 @@
+package PD3;
+
+public class PD3_2 {
+    public static void main(String[] args) {
+        byte myNumber = 58; // 1B
+        short myShort = 66; // 2B
+
+        int sum = myNumber + myShort;
+        int difference = myShort - myNumber;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
---
+++ PD3/PD3_3.java
@@ -0,0 +1,11 @@
+package PD3;
+
+public class PD3_3 {
+    public static void main(String[] args) {
+       short num1 = 10;
+       short num2 = 30;
+       short result1 = (short)(num1 + num2);
+       double muDOuble = 0.5 + 1.5f;
+
+    }
+}
